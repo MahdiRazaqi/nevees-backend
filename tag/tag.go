7d030f6c3a0043238a8bfa0f892c7dba3ae128a2
MahdiRazaqi <mahdirazaqi77@gmail.com>
2020-04-19 22:31:15 +0430

    created tag package

---
+++ tag/tag.go
@@ -0,0 +1,52 @@
+package tag
+
+import (
+	"time"
+
+	"github.com/MahdiRazaqi/nevees-backend/database"
+	"github.com/jinzhu/gorm"
+)
+
+// Tag model
+type Tag struct {
+	ID         uint      `json:"id" gorm:"primary_key"`
+	CreatedAt  time.Time `json:"created_at" gorm:"type:datetime"`
+	UpdatedAt  time.Time `json:"updated_at" gorm:"type:datetime"`
+	PostID     int       `json:"post_id" gorm:"type:int;foreignkey;not null"`
+	CategoryID int       `json:"category_id" gorm:"type:int;foreignkey;not null"`
+}
+
+func (t *Tag) table() *gorm.DB {
+	if !database.MySQL.HasTable(t) {
+		return database.MySQL.CreateTable(t)
+	}
+	return database.MySQL
+}
+
+// Insert tag to database
+func (t *Tag) Insert() error {
+	return t.table().Create(t).Error
+}
+
+// FindOne tag from database
+func (t *Tag) FindOne(order string, cond interface{}, args ...interface{}) error {
+	return t.table().Where(cond, args...).Order(order).First(t).Error
+}
+
+// Find tags from database
+func Find(limit int, page int, order string, cond interface{}, args ...interface{}) (*[]Tag, error) {
+	t := &Tag{}
+	tags := &[]Tag{}
+	err := t.table().Where(cond, args...).Order(order).Limit(limit).Offset(page - 1).Find(tags).Error
+	return tags, err
+}
+
+// Save tag from database
+func (t *Tag) Save() error {
+	return t.table().Save(t).Error
+}
+
+// Delete tag from database
+func (t *Tag) Delete() error {
+	return t.table().Delete(t).Error
+}
