337955377a1f684e048efda7e0a89f3be0bb7f76
MahdiRazaqi <Mahdirazaqi77@gmail.com>
2020-05-05 23:40:31 +0430

    public APIs method move to post API

--- web/v1/post.go
+++ web/v1/post.go
@@ -62,7 +62,7 @@ func addPost(c echo.Context) error {
  * @apiParam {Number} page list page
  * @apiParam {Number} limit list limit
  *
- * @apiSuccess {[]Object} post array of posts model
+ * @apiSuccess {[]Object} posts array of post model
  *
  * @apiError {String} error error message
  */
@@ -142,5 +142,57 @@ func removePost(c echo.Context) error {
 
 	return c.JSON(200, echo.Map{
 		"message": "post removed successfully",
+	})
+}
+
+/**
+ * @api {get} /api/v1/public/post/:id Get a post
+ * @apiVersion 1.0.0
+ * @apiName onePost
+ * @apiGroup Post
+ *
+ * @apiSuccess {Object} post post model
+ *
+ * @apiError {String} error error message
+ */
+
+func onePost(c echo.Context) error {
+	id := c.Param("id")
+
+	p := &post.Post{}
+	if err := p.FindOne("id = ?", id); err != nil {
+		return c.JSON(400, echo.Map{"error": err.Error()})
+	}
+
+	return c.JSON(200, echo.Map{
+		"post": p,
+	})
+}
+
+/**
+ * @api {get} /api/v1/public/post List posts
+ * @apiVersion 1.0.0
+ * @apiName listPosts
+ * @apiGroup Post
+ *
+ * @apiParam {Number} page list page
+ * @apiParam {Number} limit list limit
+ *
+ * @apiSuccess {[]Object} posts array of post model
+ *
+ * @apiError {String} error error message
+ */
+
+func listPosts(c echo.Context) error {
+	page, _ := strconv.Atoi(c.QueryParam("page"))
+	limit, _ := strconv.Atoi(c.QueryParam("limit"))
+
+	posts, err := post.Find(limit, page, "", "")
+	if err != nil {
+		return c.JSON(400, echo.Map{"error": err.Error()})
+	}
+
+	return c.JSON(200, echo.Map{
+		"posts": posts,
 	})
 }
